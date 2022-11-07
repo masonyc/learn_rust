87fc0fa456bcd9b3e6144052436575b8c889f779
yunc <mason.yunchen@gmail.com>
2022-11-07 22:27:16 +1300

    finish part 1

--- kvs/src/bin/kvs.rs
+++ kvs/src/bin/kvs.rs
@@ -1,3 +1,53 @@
+use kvs::KvStore;
+use std::process;
+
 fn main() {
-    println!("Hello World");
+    let _kvs = KvStore::new();
+    let cmd = clap::Command::new("kvs")
+        .bin_name("kvs")
+        .name(env!("CARGO_PKG_NAME"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand_required(true)
+        .subcommand(clap::command!("-V"))
+        .subcommand(
+            clap::command!("get").arg(clap::arg!(<KEY>).value_parser(clap::value_parser!(String))),
+        )
+        .subcommand(
+            clap::command!("rm").arg(clap::arg!(<KEY>).value_parser(clap::value_parser!(String))),
+        )
+        .subcommand(
+            clap::command!("set")
+                .arg(clap::arg!(<KEY>).value_parser(clap::value_parser!(String)))
+                .arg(clap::arg!(<VALUE>).value_parser(clap::value_parser!(String))),
+        );
+
+    let matches = cmd.get_matches();
+
+    if matches.subcommand_matches("-V").is_some() {
+        println!(env!("CARGO_PKG_VERSION"));
+    }
+
+    if let Some(_matches) = matches.subcommand_matches("get") {
+        // let k = matches.get_one::<String>("KEY");
+        eprintln!("unimplemented");
+        process::exit(1);
+        // kvs.get(k.unwrap().to_string());
+    }
+
+    if let Some(_matches) = matches.subcommand_matches("rm") {
+        // let k = matches.get_one::<String>("KEY");
+        eprintln!("unimplemented");
+        process::exit(1);
+        // kvs.remove(k.unwrap().to_string());
+    }
+
+    if let Some(_matches) = matches.subcommand_matches("set") {
+        // let k = matches.get_one::<String>("KEY");
+        // let v = matches.get_one::<String>("VALUE");
+        eprintln!("unimplemented");
+        process::exit(1);
+        // kvs.set(k.unwrap().to_string(), v.unwrap().to_string());
+    }
 }
--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,15 +1,60 @@
-pub struct KvStore {}
+#![deny(missing_docs)]
 
+//! talent plan toy kv store
+
+use std::collections::HashMap;
+
+/// KvStore that stores in memory
+pub struct KvStore {
+    /// hashmap store
+    in_memory_store: HashMap<String, String>,
+}
+
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// Provide simple get/set/remove for in memory store
+/// # Examples
 impl KvStore {
+    /// Init store with in memory HashMap
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let kv_store = KvStore::new();
+    /// ```
     pub fn new() -> KvStore {
-        KvStore {}
+        KvStore {
+            in_memory_store: HashMap::new(),
+        }
     }
 
-    pub fn set(&self, key: String, value: String) {}
+    /// set item in in_memory_store
+    ///
+    /// # Examples
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut kv_store = KvStore::new();
+    /// kv_store.set("key1".to_owned(), "value1".to_owned());
+    /// assert_eq!(kv_store.get("key1".to_owned()), Some("value1".to_owned()));
+    pub fn set(&mut self, key: String, value: String) {
+        self.in_memory_store.insert(key, value);
+        println!("{:#?}", self.in_memory_store)
+    }
 
+    ///
+    pub fn remove(&mut self, key: String) {
+        self.in_memory_store.remove(&key);
+    }
+
+    ///
     pub fn get(&self, key: String) -> Option<String> {
-        panic!()
+        self.in_memory_store.get(&key).map(|s| s.to_string())
     }
-
-    pub fn remove(&self, key: String) {}
 }
